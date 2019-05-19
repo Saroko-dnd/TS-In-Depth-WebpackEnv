6f35616b64e396cc722cdd44340e11bea5ca3f9e
saroko-dnd <isaroko@mail.ru>
2019-05-19 05:11:23 +0300

    task-1-basic-types: implemented function getAllBooks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,29 @@
 showHello('greeting', 'TypeScript');
 
 function showHello(divName: string, name: string) {
-  const elt = document.getElementById(divName);
-  elt.innerText = `Hello from ${name}`;
+    const elt = document.getElementById(divName);
+    elt.innerText = `Hello from ${name}`;
+}
+
+function getAllBooks(): any[] {
+    let books: any[] = [
+        {
+            title: 'Refactoring JavaScript',
+            author: 'Evan Burchard',
+            available: true
+        },
+        {
+            title: 'JavaScript Testing',
+            author: 'Liang Yuxian Eugene',
+            available: false
+        },
+        { title: 'CSS Secrets', author: 'Lea Verou', available: true },
+        {
+            title: 'Mastering JavaScript Object-Oriented Programming',
+            author: 'Andrea Chiarelli',
+            available: true
+        }
+    ];
+
+    return books;
 }
