b095f3f0d47a6ff92a77be17febfa50fd7a5b5fe
saroko-dnd <isaroko@mail.ru>
2019-05-22 01:32:47 +0300

    task-20-generic-constraints: created constraint on type parameter of class Shelf + created new methods for Shelf class (find and printTitles)

--- src/classes/shelf.ts
+++ src/classes/shelf.ts
@@ -1,11 +1,21 @@
-export default class Shelf<T> {
+import { ShelfItem } from '../interfaces';
+
+export default class Shelf<T extends ShelfItem> {
     private _items: T[] = [];
 
     add(item: T): void {
         this._items.push(item);
     }
 
+    find(title: string): T {
+        return this._items.find(item => item.title === title);
+    }
+
     getFirst(): T {
         return this._items[0];
+    }
+
+    printTitles(): void {
+        this._items.forEach(item => console.log(item.title));
     }
 }
