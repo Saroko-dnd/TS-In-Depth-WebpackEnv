e876a920c805fa0e892bd9756cb19847ef963142
saroko-dnd <isaroko@mail.ru>
2019-05-21 12:38:33 +0300

    task-16-default-export: created module with default export of class Encyclopedia

---
+++ src/encyclopedia.ts
@@ -0,0 +1,22 @@
+import { ReferenceItem } from './classes';
+
+class Encyclopedia extends ReferenceItem {
+    constructor(newTitle: string, newYear: number, public edition: number) {
+        super(newTitle, newYear);
+    }
+
+    printCitation(): void {
+        console.log(`${this.title}–${this.year}`);
+    }
+
+    printItem() {
+        console.log(
+            `${this.title} from ${
+                ReferenceItem.department
+            } department was published in ${this.year}`
+        );
+        console.log(`Edition: ${this.edition} (${this.year})`);
+    }
+}
+
+export default Encyclopedia;
