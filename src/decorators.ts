211b5197bb564d56f46e2dfcbfb39de11286064f
saroko-dnd <isaroko@mail.ru>
2019-05-22 04:54:25 +0300

    task-22-class-decorators-replacing-constructor-logger: created class decorator logger that replaces class constructor with another function

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,10 +1,23 @@
-function sealed(parameter: string): Function {
+function sealed(className: string): Function {
     return (constructor: Function): void => {
-        console.log(`Sealing the constructor ${parameter}`);
+        console.log(`Sealing the constructor ${className}`);
 
         Object.seal(constructor);
         Object.seal(constructor.prototype);
     };
 }
 
-export { sealed };
+function logger(target: Function): Function {
+    const newConstructor: Function = function() {
+        console.log(`Creating new instance ${target.name}`);
+
+        this.age = 30;
+    };
+
+    newConstructor.prototype = Object.create(target.prototype);
+    newConstructor.prototype.constructor = target;
+
+    return newConstructor;
+}
+
+export { sealed, logger };
